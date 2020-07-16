14a5aabce13d0e8b0774fb1db83bad02a3428199
John Uthus <john.a.uthus.jr1@gmail.com>
2020-07-15 23:09:02 -0700

    Added Instructor Class

---
+++ Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// The legal full name. First and last
+        /// Example Joe Smith
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email adress
+        /// Example: Joe.Smith@cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
